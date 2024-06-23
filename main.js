981f0c269bee4e1e345e797e4c59ea1afb664da5
Shourya Pachauri <92849643+ylosifer@users.noreply.github.com>
2024-06-23 12:37:27 +0000

    final(2)

--- main.js
+++ main.js
@@ -24,39 +24,6 @@ function changeImageBack(card) {
 // ========================== Populate Cards ====================================
 const cardData = [
     {
-        primaryImage: "//www.lotusbotanicals.com/cdn/shop/files/1_4_1200x.jpg",
-        secondaryImage: "//www.lotusbotanicals.com/cdn/shop/files/2_4_700x.jpg",
-        title: "Vitamin C Skin Brightening Face Serum",
-        description: "Deeply Hydrates Skin | 100x Vitamin C | Reduces Pigmentation",
-        price: "Rs. 599.00",
-        link: "#"
-    },
-    {
-        primaryImage: "//www.lotusbotanicals.com/cdn/shop/files/1_11_1200x.jpg",
-        secondaryImage: "//www.lotusbotanicals.com/cdn/shop/files/2_11_1200x.jpg",
-        title: "Sandalwood & Hyaluronic Acid Glow and Hydrating Sunscreen Serum | SPF50+ | PA+++",
-        description: "Ultra High Sun Protection | Unique Sunscreen Serum Formula",
-        price: "Rs. 799.00",
-        link: "#"
-    },
-    {
-        primaryImage: "//www.lotusbotanicals.com/cdn/shop/files/1_6_1200x.jpg?",
-        secondaryImage: "//www.lotusbotanicals.com/cdn/shop/files/2_4_700x.jpg",
-        title: "Vitamin C Skin Brightening Day Crème",
-        description: "Brightens Skin | Unique Gel Crème Formula | 100x Vitamin C",
-        price: "Rs. 699.00",
-        link: "#"
-
-    },
-    {
-        primaryImage: "//www.lotusbotanicals.com/cdn/shop/files/1_b22e97b2-76af-42f6-8245-95f5760c33f5_1200x.jpg",
-        secondaryImage: "//www.lotusbotanicals.com/cdn/shop/files/2_4_700x.jpg",
-        title: "Vitamin C Skin Brightening Day Crème",
-        description: "Brightens Skin | Unique Gel Crème Formula | 100x Vitamin C",
-        price: "Rs. 799.00",
-        link: "#"
-    },
-    {
         primaryImage: "//www.lotusbotanicals.com/cdn/shop/files/1_11_1200x.jpg",
         secondaryImage: "//www.lotusbotanicals.com/cdn/shop/files/2_11_1200x.jpg",
         title: "Sandalwood & Hyaluronic Acid Glow and Hydrating Sunscreen Serum | SPF50+ | PA+++",
@@ -122,6 +89,63 @@ const cardData = [
         price: "Rs. 799.00",
         link: "#"
     },
+    {
+        primaryImage: "//www.lotusbotanicals.com/cdn/shop/files/1_4_1200x.jpg",
+        secondaryImage: "//www.lotusbotanicals.com/cdn/shop/files/2_4_700x.jpg",
+        title: "Vitamin C Skin Brightening Face Serum",
+        description: "Deeply Hydrates Skin | 100x Vitamin C | Reduces Pigmentation",
+        price: "Rs. 599.00",
+        link: "#"
+    },
+    {
+        primaryImage: "//www.lotusbotanicals.com/cdn/shop/files/1_11_1200x.jpg",
+        secondaryImage: "//www.lotusbotanicals.com/cdn/shop/files/2_11_1200x.jpg",
+        title: "Sandalwood & Hyaluronic Acid Glow and Hydrating Sunscreen Serum | SPF50+ | PA+++",
+        description: "Ultra High Sun Protection | Unique Sunscreen Serum Formula",
+        price: "Rs. 799.00",
+        link: "#"
+    },
+    {
+        primaryImage: "//www.lotusbotanicals.com/cdn/shop/files/1_6_1200x.jpg?",
+        secondaryImage: "//www.lotusbotanicals.com/cdn/shop/files/2_4_700x.jpg",
+        title: "Vitamin C Skin Brightening Day Crème",
+        description: "Brightens Skin | Unique Gel Crème Formula | 100x Vitamin C",
+        price: "Rs. 699.00",
+        link: "#"
+
+    },
+    {
+        primaryImage: "//www.lotusbotanicals.com/cdn/shop/files/1_b22e97b2-76af-42f6-8245-95f5760c33f5_1200x.jpg",
+        secondaryImage: "//www.lotusbotanicals.com/cdn/shop/files/2_4_700x.jpg",
+        title: "Vitamin C Skin Brightening Day Crème",
+        description: "Brightens Skin | Unique Gel Crème Formula | 100x Vitamin C",
+        price: "Rs. 799.00",
+        link: "#"
+    },
+    {
+        primaryImage: "//www.lotusbotanicals.com/cdn/shop/files/1_11_1200x.jpg",
+        secondaryImage: "//www.lotusbotanicals.com/cdn/shop/files/2_11_1200x.jpg",
+        title: "Sandalwood & Hyaluronic Acid Glow and Hydrating Sunscreen Serum | SPF50+ | PA+++",
+        description: "Ultra High Sun Protection | Unique Sunscreen Serum Formula",
+        price: "Rs. 499.00",
+        link: "#"
+    },
+    {
+        primaryImage: "//www.lotusbotanicals.com/cdn/shop/files/1_6_1200x.jpg?",
+        secondaryImage: "//www.lotusbotanicals.com/cdn/shop/files/2_4_700x.jpg",
+        title: "Vitamin C Skin Brightening Day Crème",
+        description: "Brightens Skin | Unique Gel Crème Formula | 100x Vitamin C",
+        price: "Rs. 599.00",
+        link: "#"
+    },
+    {
+        primaryImage: "//www.lotusbotanicals.com/cdn/shop/files/1_b22e97b2-76af-42f6-8245-95f5760c33f5_1200x.jpg",
+        secondaryImage: "//www.lotusbotanicals.com/cdn/shop/files/2_4_700x.jpg",
+        title: "Vitamin C Skin Brightening Day Crème",
+        description: "Brightens Skin | Unique Gel Crème Formula | 100x Vitamin C",
+        price: "Rs. 599.00",
+        link: "#"
+    },
 
 ];
 
@@ -135,7 +159,7 @@ function createCard(cardInfo) {
     primaryImg.src = cardInfo.primaryImage;
     primaryImg.alt = "Primary Image";
     primaryImg.addEventListener('click', () => {
-        window.location.href = cardInfo.link; 
+        window.location.href = cardInfo.link;
     });
     card.appendChild(primaryImg);
 
@@ -144,7 +168,7 @@ function createCard(cardInfo) {
     secondaryImg.src = cardInfo.secondaryImage;
     secondaryImg.alt = "Secondary Image";
     secondaryImg.addEventListener('click', () => {
-        window.location.href = cardInfo.link; 
+        window.location.href = cardInfo.link;
     });
     card.appendChild(secondaryImg);
 
@@ -158,7 +182,7 @@ function createCard(cardInfo) {
     description.textContent = cardInfo.description;
     card.appendChild(description);
 
-    const price = document.createElement('p');
+    const price = document.createElement('div');
     price.classList.add('price');
     price.textContent = cardInfo.price;
     card.appendChild(price);
